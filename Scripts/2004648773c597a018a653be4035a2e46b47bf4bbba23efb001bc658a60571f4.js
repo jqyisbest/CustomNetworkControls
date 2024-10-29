be28783a737b695628d6561a13c14c314ed48080
GitHub Action <action@github.com>
2024-10-29 19:19:22 +0000

    Auto Sync For Backup

--- Scripts/2004648773c597a018a653be4035a2e46b47bf4bbba23efb001bc658a60571f4.js
+++ Scripts/2004648773c597a018a653be4035a2e46b47bf4bbba23efb001bc658a60571f4.js
@@ -11,7 +11,7 @@ if (!$response.body) {
 let obj = JSON.parse($response.body);
 
 if (url.includes("s-api.smzdm.com/sou/list_v10") && obj.data && obj.data.rows) {
-  obj.data.rows = obj.data.rows.filter((item) => item.article_tag !== "\u5e7f\u544a" || item.article_tag !== "\u6587\u7ae0");
+  obj.data.rows = obj.data.rows.filter((item) => item.article_tag !== "\u5e7f\u544a" && item.article_tag !== "\u6587\u7ae0");
 }
 
 if (url.includes("/v3/home")) {
